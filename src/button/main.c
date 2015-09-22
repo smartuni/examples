10ded6a99cb8e58a6c98967fba7b8a03b0c461a9
smlng <s@mlng.net>
2015-09-22 14:10:59 +0200

    removed unused xtimer includes

--- src/button/main.c
+++ src/button/main.c
@@ -1,8 +1,9 @@
+// written by smlng
 #include <stdio.h>
 #include "board.h"
 #include "periph/gpio.h"
-#include "xtimer.h"
 
+// button parameters, matching Atmel SAM R21 board
 #define BTN_PORT    0
 #define BTN_PIN     28
 #define BTN_PULL    GPIO_PULLUP
@@ -22,9 +23,10 @@ static void btn_callback(void* arg)
     }
 }
 
+// main
 int main(void)
 {
-    puts("Trigger Example!");
+    puts("SmartUniversity - Button Example!");
     puts("================");
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
