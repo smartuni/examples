e4830798b0c3d2dc6c305640320de4a33448f9f0
smlng <s@mlng.net>
2015-10-20 16:22:56 +0200

    coap example for riot nodes

---
+++ src/coap/endpoints.c
@@ -0,0 +1,99 @@
+#include <stdbool.h>
+#include <string.h>
+#include "board.h"
+#include "periph/gpio.h"
+#include "coap.h"
+
+// compatibility
+#ifndef LED_ON
+#define LED_ON      LED_RED_ON
+#define LED_OFF     LED_RED_OFF
+#define LED_TOGGLE  LED_RED_TOGGLE
+#endif
+
+const uint16_t rsplen = 1500;
+static char rsp[1500] = "";
+
+static char led = '0';
+
+void build_rsp(void);
+
+#include <stdio.h>
+void endpoint_setup(void)
+{
+    build_rsp();
+}
+
+static const coap_endpoint_path_t path_led = {1, {"led"}};
+static int handle_get_led(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    return coap_make_response(scratch, outpkt, (const uint8_t *)&led, 1, id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CONTENT, COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_led(coap_rw_buffer_t *scratch, const coap_packet_t *inpkt, coap_packet_t *outpkt, uint8_t id_hi, uint8_t id_lo)
+{
+    if (inpkt->payload.len == 0)
+        return coap_make_response(scratch, outpkt, NULL, 0, id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_BAD_REQUEST, COAP_CONTENTTYPE_TEXT_PLAIN);
+    if (inpkt->payload.p[0] == '1')
+    {
+        led = '1';
+        LED_ON;
+        puts("LED ON");
+        return coap_make_response(scratch, outpkt, (const uint8_t *)&led, 1, id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
+    }
+    else
+    {
+        led = '0';
+        LED_OFF;
+        puts("LED OFF");
+        return coap_make_response(scratch, outpkt, (const uint8_t *)&led, 1, id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CHANGED, COAP_CONTENTTYPE_TEXT_PLAIN);
+    }
+}
+
+const coap_endpoint_t endpoints[] =
+{
+    {COAP_METHOD_GET, handle_get_led, &path_led, "ct=0"},
+    {COAP_METHOD_PUT, handle_put_led, &path_led, NULL},
+    {(coap_method_t)0, NULL, NULL, NULL}
+};
+
+void build_rsp(void)
+{
+    uint16_t len = rsplen;
+    const coap_endpoint_t *ep = endpoints;
+    int i;
+
+    len--; // Null-terminated string
+
+    while(NULL != ep->handler)
+    {
+        if (NULL == ep->core_attr) {
+            ep++;
+            continue;
+        }
+
+        if (0 < strlen(rsp)) {
+            strncat(rsp, ",", len);
+            len--;
+        }
+
+        strncat(rsp, "<", len);
+        len--;
+
+        for (i = 0; i < ep->path->count; i++) {
+            strncat(rsp, "/", len);
+            len--;
+
+            strncat(rsp, ep->path->elems[i], len);
+            len -= strlen(ep->path->elems[i]);
+        }
+
+        strncat(rsp, ">;", len);
+        len -= 2;
+
+        strncat(rsp, ep->core_attr, len);
+        len -= strlen(ep->core_attr);
+
+        ep++;
+    }
+}
--- src/coap/main.c
+++ src/coap/main.c
@@ -17,17 +17,12 @@
 #include "periph/gpio.h"
 #include "shell.h"
 #include "thread.h"
+#include "coap.h"
 
-#include "coap_example.h"
+// parameters
+#define COAP_BUF_SIZE         (63)
+#define COAP_PORT             (24911)
 
-// array with available shell commands
-static const shell_command_t shell_commands[] = {
-    { "coap", "send get request", coap_cmd },
-    { NULL, NULL, NULL }
-};
-
-extern void start_receiver(void);
-extern int coap_cmd(int argc, char **argv);
 /**
  * @brief the main programm loop
  *
@@ -42,13 +37,73 @@ int main(void)
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
     puts("================");
 
-    // start udp receiver
-    start_receiver();
+    // init coap endpoints
+    endpoint_setup();
 
-    // start shell
-    puts("All up, running the shell now");
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    // start coap listener
+    struct sockaddr_in6 server_addr;
+    char src_addr_str[IPV6_ADDR_MAX_STR_LEN];
+    uint16_t port;
+    static int sock = -1;
+    static uint8_t buf[COAP_BUF_SIZE];
+    uint8_t scratch_raw[COAP_BUF_SIZE];
+    coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
+
+    sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    /* parse port */
+    port = (uint16_t)COAP_PORT;
+    if (port == 0) {
+        puts("Error: invalid port specified");
+        return 1;
+    }
+    server_addr.sin6_family = AF_INET6;
+    memset(&server_addr.sin6_addr, 0, sizeof(server_addr.sin6_addr));
+    server_addr.sin6_port = htons(port);
+    if (sock < 0) {
+        puts("error initializing socket");
+        sock = 0;
+        return 1;
+    }
+    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        sock = -1;
+        puts("error binding socket");
+        return 1;
+    }
+    printf("Success: started COAP listener on port %" PRIu16 "\n", port);
+    while (1) {
+        int res,rc;
+        struct sockaddr_in6 src;
+        socklen_t src_len = sizeof(struct sockaddr_in6);
+        coap_packet_t pkt;
+        // blocking receive, waiting for data
+        if ((res = recvfrom(sock, buf, sizeof(buf), 0,
+                            (struct sockaddr *)&src, &src_len)) < 0) {
+            puts("Error on receive");
+        }
+        else if (res == 0) {
+            puts("Peer did shut down");
+        }
+        else { // check for PING or PONG
+            if (0 != (rc = coap_parse(&pkt, buf, res)))
+                printf("Bad packet rc=%d\n", rc);
+            else
+            {
+                inet_ntop(AF_INET6, &(src.sin6_addr),
+                          src_addr_str, sizeof(src_addr_str));
+                printf(". received COAP message from [%s].\n", src_addr_str);
+                size_t rsplen = sizeof(buf);
+                coap_packet_t rsppkt;
+                coap_handle_req(&scratch_buf, &pkt, &rsppkt);
+
+                if (0 != (rc = coap_build(buf, &rsplen, &rsppkt))) {
+                    printf("coap_build failed rc=%d\n", rc);
+                }
+                else {
+                    sendto(sock, buf, rsplen, 0, (struct sockaddr *)&src, src_len);
+                }
+            }
+        }
+    }
 
     // should be never reached
     return 0;
